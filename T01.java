be45d52af366bb5feb6fea710c1c7a3640abf8d7
SilviaSitorus <silviaeklesiana12@gmail.com>
2024-09-06 11:17:15 +0700

    Kami dari pair 9 telah menyelesaikan tugas dengan baik

--- T01.java
+++ T01.java
@@ -1,1 +1,37 @@
-// NIM - Your Name
+// 12S24004 - Silvia Eklesiana Br. Sitorus
+// 12S24032 - Angga Brillyant Pratama Sianipar
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        int tahun;
+
+        tahun = input.nextInt();
+        String penerbit;
+
+        penerbit = input.nextLine();
+        String format;
+
+        format = input.nextLine();
+        double harga, margin;
+
+        harga = input.nextDouble();
+        margin = input.nextDouble();
+        int stock;
+
+        stock = input.nextInt();
+        double rating;
+
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahun + "|" + penerbit + "|" + format + "|" + harga + "|" + margin + "|" + stock + "|" + rating);
+    }
+}
